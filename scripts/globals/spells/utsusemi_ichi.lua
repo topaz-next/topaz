1cb7e80e26c1b931ae028b0a1e9c41983056f172
gweivyth <37130689+gweivyth@users.noreply.github.com>
2021-02-02 12:11:50 -0500

    Update utsusemi_ichi.lua

--- scripts/globals/spells/utsusemi_ichi.lua
+++ scripts/globals/spells/utsusemi_ichi.lua
@@ -9,7 +9,7 @@ function onMagicCastingCheck(caster,target,spell)
     return 0
 end
 
-function onSpellCast(caster,target,spell)
+function onSpellCast(caster, target, spell)
     local effect = target:getStatusEffect(tpz.effect.COPY_IMAGE)
 	and target:delStatusEffect(67) -- Third Eye and Utsusemi don't stack.  Utsusemi removes Third Eye.
     
