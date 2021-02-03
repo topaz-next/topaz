d566328edb1183ad3f5d5cd318ec6259f15f846f
gweivyth <37130689+gweivyth@users.noreply.github.com>
2021-02-03 13:37:03 -0500

    Formatting
    Thanks Xaver

--- scripts/globals/spells/utsusemi_ichi.lua
+++ scripts/globals/spells/utsusemi_ichi.lua
@@ -5,13 +5,13 @@ require("scripts/globals/status")
 require("scripts/globals/msg")
 -----------------------------------------
 
-function onMagicCastingCheck(caster,target,spell)
+function onMagicCastingCheck(caster, target, spell)
     return 0
 end
 
 function onSpellCast(caster, target, spell)
     local effect = target:getStatusEffect(tpz.effect.COPY_IMAGE)
-	and target:delStatusEffect(67) -- Third Eye and Utsusemi don't stack.  Utsusemi removes Third Eye.
+    and target:delStatusEffect(67) -- Third Eye and Utsusemi don't stack.  Utsusemi removes Third Eye.
     
 	-- Get extras shadows
     local numShadows = 3 + target:getMod(tpz.mod.UTSUSEMI_BONUS)
@@ -29,3 +29,4 @@ function onSpellCast(caster, target, spell)
 
     return tpz.effect.COPY_IMAGE
 end
+
