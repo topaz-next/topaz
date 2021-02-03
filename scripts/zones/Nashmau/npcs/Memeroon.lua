064a2f8f3b1f0ff8e937f56d0314b1ece046c7d2
Zach Toogood <zrtoogood@gmail.com>
2021-02-03 11:21:10 +0200

    Update Memeroon

--- scripts/zones/Nashmau/npcs/Memeroon.lua
+++ scripts/zones/Nashmau/npcs/Memeroon.lua
@@ -1,21 +1,30 @@
 -----------------------------------
 -- Area: Nashmau
 --  NPC: Memeroon
--- Standard Info NPC
+-- Type: Appraiser
+-- !pos -26.3 0 -40.6 48
+-----------------------------------
+require("scripts/globals/utils/appraisal")
+require("scripts/globals/npc_util")
+local ID = require("scripts/zones/Nashmau/IDs")
 -----------------------------------
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
+    -- TODO: Check this ID (273)
+    appraisalUtil.appraiseItem(player, npc, trade, 500, 273)
 end
 
 entity.onTrigger = function(player, npc)
-    player:startEvent(272)
+    player:startEvent(272, 500)
 end
 
 entity.onEventUpdate = function(player, csid, option)
 end
 
-entity.onEventFinish = function(player, csid, option)
+entity.onEventFinish = function(player, csid, option, npc)
+    -- TODO: Check this ID (273)
+    appraisalUtil.appraisalOnEventFinish(player, csid, option, 500, 273, npc)
 end
 
 return entity
