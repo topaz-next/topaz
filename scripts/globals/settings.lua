c9175ca22f157392bdf32d5560b3b97096a27369
kaincenteno <kain.centeno@catsangel.com>
2021-02-23 17:35:14 -0800

    Login Campaign Adds logic for adding points if campaign is active. Makes Greeter Moogles show list of items to trade points for. Shows messages when zoning/logging in about Campaign.
    Only three past campaigns were added, defaulting to December's but it won't run
    as that campaign was only meant to run for 23 days.
    
    Will be adding most recent campaigns on a different PR

--- scripts/globals/settings.lua
+++ scripts/globals/settings.lua
@@ -51,6 +51,10 @@ CAP_CURRENCY_VALOR     = 50000
 -- Magian Trials
 ENABLE_MAGIAN_TRIALS = 1
 
+-- Login Campaign (Set to 0 if you don't want to run a Login Campaign)
+-- Please visit scripts/globals/events/login_campaign.lua for assigning the correct campaign.
+ENABLE_LOGIN_CAMPAIGN = 1
+
 -- TREASURE CASKETS
 -- Retail droprate = 0.1 (10%) with no other effects active
 -- Set to 0 to disable caskets.
